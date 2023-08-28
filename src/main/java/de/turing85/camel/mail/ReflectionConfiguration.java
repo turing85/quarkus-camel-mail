c0259ef4d5aa337739fbe47eba6f0c2a3005ce8f
Marco Bungart <marco.bungart@googlemail.com>
2023-08-28 18:33:41 +0200

    update reflection configuration to also reference jakarta instead of javax

--- src/main/java/de/turing85/camel/mail/ReflectionConfiguration.java
+++ src/main/java/de/turing85/camel/mail/ReflectionConfiguration.java
@@ -3,7 +3,7 @@ package de.turing85.camel.mail;
 //import io.quarkus.runtime.annotations.RegisterForReflection;
 //
 //@RegisterForReflection(targets = {
-//    javax.mail.internet.AddressException.class
+//    jakarta.mail.internet.AddressException.class
 //})
 @SuppressWarnings("unused")
 public class ReflectionConfiguration {
