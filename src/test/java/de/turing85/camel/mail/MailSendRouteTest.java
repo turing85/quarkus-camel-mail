10939d7922b795089d4cf2e2cea60291ecbcc349
Marco Bungart <marco.bungart@googlemail.com>
2023-10-06 18:59:02 +0200

    Use greenmail test extension

--- src/test/java/de/turing85/camel/mail/MailSendRouteTest.java
+++ src/test/java/de/turing85/camel/mail/MailSendRouteTest.java
@@ -1,30 +1,49 @@
 package de.turing85.camel.mail;
 
-import de.turing85.camel.mail.resource.GreenmailTestResourceLifecycleManager;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.io.IOException;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
-@QuarkusTestResource(GreenmailTestResourceLifecycleManager.class)
 class MailSendRouteTest {
+  @RegisterExtension
+  public static final GreenMailExtension GREEN_MAIL =
+      new GreenMailExtension(ServerSetupTest.SMTP);
+
   @Test
-  void testSendMail() {
+  void testSendMail() throws MessagingException, IOException {
     // @formatter:off
-    given()
+      String expectedRecipient = "foo@bar.baz";given()
         .contentType(ContentType.TEXT)
-        .body("foo@bar.baz")
+        .body(expectedRecipient)
         .when()
           .post("/send")
         .then()
           .statusCode(is(HttpResponseStatus.OK.code()))
           .body(is("Hello"));
     // @formatter:on
+    List<MimeMessage> messages = List.of(GREEN_MAIL.getReceivedMessages());
+    assertThat(messages).hasSize(1);
+    MimeMessage message = messages.get(0);
+    assertThat(message.getRecipients(Message.RecipientType.TO)).hasLength(1);
+    assertThat(message.getRecipients(Message.RecipientType.TO)[0].toString())
+        .isEqualTo(expectedRecipient);
+    assertThat(message.getSubject()).isEqualTo("important");
+    assertThat(message.getContent()).isEqualTo("Hello");
   }
 
   @Test
